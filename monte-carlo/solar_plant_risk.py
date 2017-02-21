ea92bd60b4090a2bfc8aa4c6b24af68ee12a9e64
Youngji, Cho <cyj9058@gmail.com>
2017-02-21 22:40:20 +0900

    Update solar_plant_risk.py

--- monte-carlo/solar_plant_risk.py
+++ monte-carlo/solar_plant_risk.py
@@ -1,7 +1,5 @@
 import os 
 os.chdir("/Users/joyeongji/Dropbox/data")
-
-import openpyxl as xl
 import numpy as np
 import pandas as pd 
 import scipy as sp 
@@ -19,25 +17,8 @@ efficiency=1
 def revenue(capa,year,smp,rec,runtime,efficiency):
     time=1
     revenue_stream=pd.Series(list(range(1,year*12+1)))   
-    while time<=year*12:
+    while time<=year*12: 
         revenue_stream[time]=capa*(smp+rec)*runtime*efficiency
-
-
-
-revenue_stream=pd.Series([1,5])
-
-
-
-def random_risk_generate(risk_type, ):
-    if(risk_type=='normal' )
-
-    
-s1 = pd.Series([1, 2, 3])
-s2 = pd.Series([4, 5, 6])
-s3 = pd.Series([4, 5, 6], index=[3,4,5])
-s1.append(s2)
-
-revenue_stream=pd.Series(list(range(1,241)))    
-    
-    
-    sp.stats.binom(N, theta)
+        time +=1
+        efficiency *=0.999     
+    return(revenue_stream)
