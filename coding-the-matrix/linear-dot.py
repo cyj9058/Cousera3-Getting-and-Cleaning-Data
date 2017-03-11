cd73e6d2106bd1ac1b96783ceaf3cd522a0aef15
Youngji, Cho <cyj9058@gmail.com>
2017-03-11 16:24:44 +0900

    Create linear-dot.py

---
+++ coding-the-matrix/linear-dot.py
@@ -0,0 +1,12 @@
+import numpy as np 
+import numpy.linalg as lin
+
+a=np.array([[1,0],[0,1]])
+b=np.array([[4,1],[2,2]])
+
+np.dot(a,b) #dot 
+np.cross(a,b) #cross product
+np.inner(a,b)#inner product 
+np.outer(a,b)
+np.tensordot(a,b)
+np.trace(a)
